b5195988a4a26ff28630dbfc1476974aa4af606c
pkakar00 <pkakar00@mylangara.ca>
2024-05-08 23:59:22 -0700

    Fixing errors-

--- apps/web/components/ChatScreen.tsx
+++ apps/web/components/ChatScreen.tsx
@@ -1,6 +1,6 @@
 "use client";
 // eslint-disable-next-line turbo/no-undeclared-env-vars
-let url = process.env.WEBSITE_URL || "http://localhost:3000/";
+let url = process.env.NEXT_PUBLIC_WEBSITE_URL || "http://localhost:3000/";
 import { useCallback, useContext, useEffect, useState } from "react";
 import { SelectedUserContext } from "./client-wrapper/ClientComponentContext";
 import { ChatMessage, User } from "@repo/prisma-db";
